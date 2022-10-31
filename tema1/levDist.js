11f162bd94048fb3ac2fb833412ba93b10eb4073
maarria777 <ivan2maria20@stud.ase.ro>
2022-10-31 20:50:10 +0200

    tema1

--- tema1/levDist.js
+++ tema1/levDist.js
@@ -1,40 +1,36 @@
 const distance = (a, b) => {
-	if (!(typeof (a) === 'string' || typeof (a) === 'String'))
-		throw Error('InvalidType');
-	if (typeof (b) != 'string' && typeof (b) != 'String')
-		throw Error('InvalidType');
-	n = a.length; a = a.split("");
-	m = b.length; b = b.split("");
-	let dist = [];
-	cost = 0;
-	if (n == 0 && m == 0) { return 0; }
-	if (n == 0 && m != 0) {
-		d = m;
-		return d;
-	}
-	if (n != 0 && m == 0) {
-		d = n;
-		return d;
-	}
-	const res = Array(m + 1).fill(null).map(() =>
-		Array(n + 1).fill(null));
-	for (let i = 0; i <= n; i += 1) {
-		res[0][i] = i;
-	}
-	for (let j = 0; j <= m; j += 1) {
-		res[j][0] = j;
-	}
-	for (let j = 1; j <= m; j += 1) {
-		for (let i = 1; i <= n; i += 1) {
-			const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
-			res[j][i] = Math.min(
-				res[j][i - 1] + 1,
-				res[j - 1][i] + 1,
-				res[j - 1][i - 1] + indicator,
-			);
+		let k=0
+		if(typeof a!=="string" || typeof b!=="string"){
+			throw new Error("InvalidType") 
+		}else{
+			if(a.length<b.length){
+				k=b.length
+			}else if(b.length<a.length){
+				k=a.length
+			}
+
+			const matrice=[]
+			for(let i=0;i<k+1;i++){ //+1 pentru empty string
+				const rand=[];
+				for(let j=0;j<k+1;j++){
+					rand.push(j)
+				}
+				rand[0]=i
+				matrice.push(rand)
+			}
+	
+			for(let i=1;i<k+1;i++){
+				for(let j=1;j<k+1;j++){
+					if(a[i-1]===b[j-1]){
+						matrice[i][j]=matrice[i-1][j-1]
+					}else{
+						matrice[i][j]=Math.min(matrice[i-1][j],matrice[i-1][j-1],matrice[i][j-1])+1
+					}
+				}
+			}
+
+			return matrice[a.length][b.length]
 		}
-	}
-	return res[m][n];
 
 
 }
