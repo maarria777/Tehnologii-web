35bc1c674a6a28451b363223791445207db5dc60
maarria777 <ivan2maria20@stud.ase.ro>
2023-01-11 05:37:37 +0200

    tema4

---
+++ tema4/build/precache-manifest.4579185a17a462343e6fb16a71c1424c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "149e8f6b270ef7f1e2e6",
+    "url": "/static/js/main.149e8f6b.chunk.js"
+  },
+  {
+    "revision": "af49b1ea27d1d39597d1",
+    "url": "/static/js/1.af49b1ea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "069e95404330b3c1872223fb5c6c47ab",
+    "url": "/index.html"
+  }
+];
